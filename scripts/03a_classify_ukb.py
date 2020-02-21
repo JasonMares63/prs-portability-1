810bf754b23cc21946e562296bc480f0693c89f5
Michael Zietz <michael.zietz@gmail.com>
2020-02-20 23:20:10 -0500

    Consolidate population classification and separation scripts (#6)
    Merges https://github.com/zietzm/prs-portability/pull/6
    * Consolidate classification and population separation scripts
    * Removes pgen creation for each predicted population in the UK BB (not needed later)

--- scripts/03a_classify_ukb.py
+++ scripts/03a_classify_ukb.py
@@ -1,33 +1,42 @@
+import pickle
+
 import pandas as pd
 import sklearn.ensemble
 
 
 def load_1000_genomes(pc_cols):
+    """
+    Loads training data from the 1000 Genomes Project. Individuals are labelled
+    only by their population codes, so a map must be used to aggregate
+    populations into super populations.
+    """
     # 1000 Genomes principal components file
     kgp_pc_df = pd.read_csv(
-        'data/merged/projection.sscore', 
+        'data/kgp_merged/projection.sscore',
         sep='\t',
         usecols=['#FID', 'IID', *pc_cols]
     )
     # 1000 Genomes individual population labels
     kgp_pop_df = pd.read_csv(
-        'data/integrated_call_samples_v2.20130502.ALL.ped', 
-        sep='\t',  
+        'data/integrated_call_samples_v2.20130502.ALL.ped',
+        sep='\t',
         usecols=['Family ID', 'Individual ID', 'Gender', 'Population']
     )
     # 1000 Genomes map between population and super population
     kgp_super_pop_df = pd.read_csv(
-        'data/20131219.populations.tsv', 
-        sep='\t', 
+        'data/20131219.populations.tsv',
+        sep='\t',
         usecols=['Population Code', 'Super Population']
     )
 
     # Combine DataFrames to train classifier
     kgp_merged = (
         kgp_pop_df
-        .merge(kgp_super_pop_df, left_on='Population', right_on='Population Code', how='inner')
+        .merge(kgp_super_pop_df, left_on='Population',
+               right_on='Population Code', how='inner')
         .merge(kgp_pc_df, left_on='Individual ID', right_on='IID', how='inner')
-        .filter(items=['IID', 'Population', 'Super Population', 'Gender', *pc_cols])
+        .filter(items=['IID', 'Population', 'Super Population',
+                       'Gender', *pc_cols])
     )
     return kgp_merged
 
@@ -36,19 +45,18 @@ def train_classifier(df, pc_cols, n_estimators=100, seed=100):
     # Separate data into X and y, as typical in the sklearn API
     principal_components = df.loc[:, pc_cols].values
     labels = df.loc[:, 'Super Population'].values
-    
-    rf = sklearn.ensemble.RandomForestClassifier(n_estimators=n_estimators, 
+
+    rf = sklearn.ensemble.RandomForestClassifier(n_estimators=n_estimators,
                                                  random_state=seed)
     rf.fit(principal_components, labels)
     return rf
 
 
-def classify_ukbb(classifier, pc_cols, output_file='data/ukb_filtered/population_labels.tsv.gz', 
-                  min_prob=0.9):
+def classify_ukbb(classifier, pc_cols, min_prob=0.9):
     # Load UK Biobank PCA data and store as two arrays (to save memory)
     ukb_pc_df = pd.read_csv(
-        'data/ukb_filtered/projection.sscore', 
-        sep='\t', 
+        'data/ukb_merged/projection.sscore',
+        sep='\t',
         usecols=['#FID', 'IID', *pc_cols]
     )
     sample_ids = ukb_pc_df.loc[:, ['#FID', 'IID']]
@@ -72,17 +80,21 @@ def classify_ukbb(classifier, pc_cols, output_file='data/ukb_filtered/population
 
     for i, label in enumerate(classifier.classes_):
         ukb_labels_df[label] = probabilities[:, i]
+    return ukb_labels_df
 
-    ukb_labels_df.to_csv(output_file, sep='\t', compression='infer', index=False)
 
-    
 if __name__ == '__main__':
-    # Which PCs to use
+    # Which PCs to use for training the population classifier
     use_pcs = [f'PC{i}_AVG' for i in range(1, 11)]
 
     # Load 1000 Genomes data (only those PCs we want to use)
     kgp_df = load_1000_genomes(use_pcs)
     classifier = train_classifier(kgp_df, use_pcs)
     del kgp_df
-    classify_ukbb(classifier, use_pcs, output_file='data/ukb_filtered/population_labels_10PCS.tsv.gz')
 
+    with open('data/population_classifier.pkl', 'wb') as f:
+        pickle.dump(classifier, f)
+
+    ukb_labels_df = classify_ukbb(classifier, use_pcs)
+    ukb_labels_df.to_csv('data/ukb_merged/population_labels_10PCS.tsv.gz',
+                         sep='\t', compression='infer', index=False)
--- scripts/03b_separate_populations.py
+++ scripts/03b_separate_populations.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+import pandas as pd
 
 
 def make_population_sample_files(all_populations_df):
@@ -8,17 +8,18 @@ def make_population_sample_files(all_populations_df):
             all_populations_df
             .query(f'predicted == "{population}"')
             .loc[:, ['#FID', 'IID']]
-            .to_csv(f'data/ukb_populations/{population}_all.txt', header=True, index=False, sep=' ')
+            .to_csv(f'data/ukb_populations/{population}_all.txt', header=True,
+                    index=False, sep=' ')
         )
-        
-        
+
+
 def get_trait_to_n_samples():
     """
     Each trait uses different number of EUR for GWAS (to enable comparison
-    with BBJ). Sample these numbers from the EUR DataFrame and save them as 
+    with BBJ). Sample these numbers from the EUR DataFrame and save them as
     sample files for each trait.
     """
-    martin_gwas_info = pd.read_csv('data/martin_gwas_info.txt', sep=' ')
+    martin_gwas_info = pd.read_csv('data/martin_gwas_info.txt', sep=r'\s+')
 
     trait_to_n_samples = (
         martin_gwas_info
@@ -27,37 +28,47 @@ def get_trait_to_n_samples():
         .to_dict()
     )
     return trait_to_n_samples
-    
+
 
 def make_trait_sample_files(all_samples_df, trait_to_n_samples, seed=100):
     """
-    Sample the appropriate number of 
+    Sample the appropriate number of EUR population individuals for the GWAS
+    of each trait. As per Martin et al.'s method, the number of individuals
+    used for the GWAS is dependent on the number of individuals used in the
+    equivalent-trait GWAS in Biobank Japan.
     """
     for trait, n_samples in trait_to_n_samples.items():
         (
             all_samples_df
             .sample(n=n_samples, random_state=seed)
-            .to_csv(f'data/ukb_populations/{trait}.txt', header=True, index=False, sep=' ')
+            .to_csv(f'data/ukb_populations/{trait}.txt', header=True,
+                    index=False, sep=' ')
         )
-        
-        
+
+
 if __name__ == '__main__':
     seed = 100
 
     # DataFrame of predicted super population labels for UKBB samples
     labels_df = (
-        pd.read_csv('data/ukb_filtered/population_labels_10PCS.tsv.gz', sep='\t')
+        pd.read_csv('data/ukb_merged/population_labels_10PCS.tsv.gz', sep='\t')
         # .query('inconclusive == False')
         .drop_duplicates(subset=['#FID', 'IID'])
     )
 
-    # 5000 Samples used as the "Target" for EUR in 
+    # 5000 random samples used as the "Target" for EUR
     eur_test_df = (
         labels_df
         .query('predicted == "EUR"')
         .sample(n=5000, replace=False, random_state=seed)
     )
-    eur_test_df.loc[:, ['#FID', 'IID']].to_csv('data/ukb_populations/EUR_test.txt', index=False, header=True, sep=' ')
+
+    (
+        eur_test_df
+        .loc[:, ['#FID', 'IID']]
+        .to_csv('data/ukb_populations/EUR_test.txt', index=False, header=True,
+                sep=' ')
+    )
 
     # All labels for all populations (5000 test EUR removed)
     non_test_df = labels_df.drop(eur_test_df.index, inplace=False)
@@ -78,4 +89,3 @@ if __name__ == '__main__':
 
     # Create a sample file for each trait (correct number of samples, all EUR)
     make_trait_sample_files(eur_train_df, trait_to_n_samples, seed=seed)
-
